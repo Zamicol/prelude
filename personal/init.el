e610c899d528d492e4b7fd1dccde774d6b4dd389
Zach Collier <zachmcollier@gmail.com>
2015-02-06 16:40:07 -0700

    swapped sppedbar for sr-speedbar

--- personal/init.el
+++ personal/init.el
@@ -1,8 +1,19 @@
+;start with the menu bar open.  
 (menu-bar-mode 1)
 
 ; start speedbar if we're using a window system
+;(when window-system
+;	(speedbar t))
+
+;sr-speedbar is better than speedbar.  
+(setq 
+	sr-speedbar-right-side nil
+	sr-speedbar-width-x 10
+	sr-speedbar-width-console 10
+	sr-speedbar-max-width 10)
+
 (when window-system
-	(speedbar t))
+	(sr-speedbar-open))
 
 ;Move the default directory for eshell alias
 ;Allows me to save the eshell settings.  
