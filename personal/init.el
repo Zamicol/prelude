4caf89f81b3777c17fb3c70a55b70c78685e262a
Zach Collier <zachmcollier@gmail.com>
2015-02-06 11:21:08 -0700

    move eshell to personal folder

--- personal/init.el
+++ personal/init.el
@@ -1,4 +1,10 @@
 (menu-bar-mode 1)
+
 ; start speedbar if we're using a window system
 (when window-system
 	(speedbar t))
+
+;Move the default directory for eshell alias
+;Allows me to save the eshell settings.  
+(setq eshell-directory-name "~/.emacs.d/personal/eshell")
+
